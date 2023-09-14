9b1b83f6f4627e2899a1bd65951796a76a4d41c8
Aadish Bansal <aadibansal901@gmail.com>
2023-09-14 16:09:08 +0530

    added project

---
+++ server.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const app = require("./app");
+
+const PORT = 8080;
+
+mongoose
+  .connect(
+    "mongodb+srv://aadish:lUP2RfMufQPyLgtX@cluster0.77ot1dz.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp/book-management-service"
+  )
+  .then((client) => {
+    console.log(client);
+    app.listen(PORT, () => console.log("server running"));
+  })
+  .catch((err) => console.log(err));
